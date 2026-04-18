538b7af791ae75aae332bbc83f26ebdfb24198be
rootquan <530637626@qq.com>
2026-04-18 15:30:31 +0800

    One-click theme switch

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,7 +8,7 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "退出 Neovim" })
 
 -- 可选：绑定 <leader>Q 为强制退出（不保存）
---vim.keymap.set("n", "<leader>Q", "<cmd>q!<CR>", { desc = "强制退出 Neovim" })
+vim.keymap.set("n", "<leader>Q", "<cmd>q!<CR>", { desc = "强制退出 Neovim" })
 
 -- 可选：绑定 <leader>wq 为保存并退出
 vim.keymap.set("n", "<leader>wq", "<cmd>wq<CR>", { desc = "保存并退出" })
@@ -36,6 +36,16 @@ vim.api.nvim_create_autocmd("FileType", {
 		end, { buffer = true, desc = "关闭Telescope窗口" })
 	end,
 })
+
+-- 在 keymaps.lua 里加
+local themes = { "catppuccin", "solarized-osaka" }
+local current = 1
+
+vim.keymap.set("n", "<leader>tt", function()
+	current = current % #themes + 1
+	vim.cmd.colorscheme(themes[current])
+	print("Switched to: " .. themes[current])
+end, { desc = "切换主题" })
 
 -- ==============================================
 -- 项目列表快捷键：空格+p 打开
